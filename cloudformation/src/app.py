47f631da2c694cf70e3d6fed552d5696dc7ccbf0
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-02-19 21:50:54 +0900

    loggerのサンプル追加

--- cloudformation/src/app.py
+++ cloudformation/src/app.py
@@ -1,2 +1,17 @@
-def main():
-    print("Hellopppp")
+import json
+import logging
+
+
+def lambda_handler(event, context):
+    format = logging.Formatter('%(asctime)s %(levelname)s %(error_code)s [%(module_name)s] %(message)s', '%Y-%m-%dT%H:%M:%SZ')
+    logger = logging.getLogger()
+    
+    for handler in logger.handlers:
+        handler.setFormatter(format)
+    
+    logger.setLevel(logging.DEBUG)
+    logger.debug('debug log @@@', extra={'error_code':'0002', 'module_name':'external papa'})
+    logger.info('info log @@@', extra={'error_code':'0002', 'module_name':'external papa'})
+    logger.warn('warn log @@@', extra={'error_code':'0002', 'module_name':'external papa'})
+    logger.error('error log @@@', extra={'error_code':'0002', 'module_name':'external papa'})
+    logger.critical('critical log @@@', extra={'error_code':'0002', 'module_name':'external papa'})

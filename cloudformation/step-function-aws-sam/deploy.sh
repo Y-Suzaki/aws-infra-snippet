52486766ff6f79d05f2c6464aa963eb9b168fc11
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-08-06 22:26:07 +0900

    test

---
+++ cloudformation/step-function-aws-sam/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+s3_bucket="ys-dev-web-codepipeline-artifacts"
+
+echo "** Start to deploy and build. **"
+
+echo "Build serverless function..."
+aws cloudformation package \
+  --template-file aws-sam.yaml \
+  --output-template-file aws-sam-deploy.yaml \
+  --s3-bucket ${s3_bucket} \
+  --s3-prefix serverless-function \
+  --region us-west-2 \
+  --profile default
+
+echo "Deploy serverless function..."
+aws cloudformation deploy \
+  --template-file aws-sam-deploy.yaml \
+  --stack-name serverless-function \
+  --capabilities CAPABILITY_IAM \
+  --region us-west-2 \
+  --profile default
+
+echo "** All complete! **"
+aws s3 rm s3://${s3_bucket}/serverless-function/ \
+  --region us-west-2 \
+  --profile default \
+  --recursive
+
+rm -f aws-sam-deploy.yaml
